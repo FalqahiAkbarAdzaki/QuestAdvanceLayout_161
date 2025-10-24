fe38c242a922b5ed75fea2b89424a6a6ed1d91df
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-10-24 10:13:19 +0700

    eleventh commit

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column

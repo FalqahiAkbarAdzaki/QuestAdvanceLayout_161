003acf8c13d2e0cecddb677324ec2e66ce6c2b2e
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-10-24 10:13:00 +0700

    tenth commit

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -1,9 +1,14 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,13 +16,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,58 +35,49 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
 
-
-
-
-
-
-
-
-
-
-
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    column(
+    Column (
         modifier = Modifier
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource(R.string.prodi),
-            fontSize = 35.sp
-            fontWeight = Fontweight.Bold
+            stringResource(R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
         )
         Text(
-            stringResource(R.string.univ)
+            stringResource(R.string.univ),
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(25.dp))
         Card(
             modifier = Modifier
                 .fillMaxWidth(1f)
-                .padding(12.dp)
+                .padding(12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
         ) {
             Row() {
-                val gambar = painterResource(R.drawable.logo_umy)
-                image(
-                    painter = gambar
-                            contentDescription = null,
+                val gambar = painterResource(id = R.drawable.logo_umy)
+
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
                     modifier = Modifier
                         .size(100.dp)
                 )
                 Spacer(modifier = Modifier.width(50.dp))
                 Column() {
                     Text(
-                        stringResource(R.string.nama)
-                                fontSize = 30.sp
-                                fontFamily = FontFamily . Cursive
-                                color = Color.White
-                                modifier = Modifier . padding (top = 15.dp)
+                        stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
                     )
                 }
                 Box(
@@ -84,11 +85,13 @@ fun ActivitasPertama(modifier: Modifier){
                         .fillMaxSize()
                 ) {
                     Text(
-                        stringResource(R.string.copy)
-                                modifier = Modifier
-                                . align (Alignment.BottomCenter)
+                        stringResource(R.string.copy),
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
                             .padding(bottom = 50.dp)
                     )
                 }
             }
         }
+    }
+}

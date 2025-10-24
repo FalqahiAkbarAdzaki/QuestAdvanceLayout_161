f0873f9eb8d601b1aa22201d930054da47023cea
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-10-24 10:14:14 +0700

    thirteenth commit

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.provider.CalendarContract
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
